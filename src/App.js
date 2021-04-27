294658f3b84e1fbcf1e254146b5e398142477faf
arfa <arfrix.dev@gmail.com>
2021-04-27 16:54:56 +0430

    tailwind added in other to close #13 issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    
   );
 }
 
---
+++ src/layouts/homeLayout/HomeLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../../blocks/header/Header'
+import Footer from '../../blocks/footer/Footer'
+
+export default function HomeLayout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      
+    </div>
+  )
+}
