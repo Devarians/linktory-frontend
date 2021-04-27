aa3c15b94da04e0fbe9229293f69d5ec17f3382b
arfa <arfrix.dev@gmail.com>
2021-04-27 17:03:53 +0430

    basic structure added | #8

--- src/layouts/homeLayout/HomeLayout.js
+++ src/layouts/homeLayout/HomeLayout.js
@@ -5,7 +5,9 @@ import Footer from '../../blocks/footer/Footer'
 export default function HomeLayout() {
   return (
     <div className="flex flex-col min-h-screen">
-      
+      <Header className="flex-grow-0"/>
+      <main className="container flex-grow"></main>
+      <Footer className="flex-grow-0"/>
     </div>
   )
 }
